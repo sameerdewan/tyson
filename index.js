153b72f58033addd7f7833cf010534e233649ae3
sameerdewan <sameer.dewan@itential.com>
2020-11-08 10:07:20 -0500

    expose chunk

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { call } = require('./lib/call/call');
 const { attempt } = require('./lib/attempt/attempt');
+const { chunk } = require('./lib/chunk/chunk');
 
 module.exports = {
   call,
   attempt,
+  chunk,
 };
