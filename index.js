d9dfd619ddceaca302d2cb9d307ae7dfbe9814c4
sameerdewan <sameer.dewan@itential.com>
2020-11-08 21:55:07 -0500

    expose asyncSequence

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { call } = require('./lib/call/call');
 const { attempt } = require('./lib/attempt/attempt');
 const { chunk } = require('./lib/chunk/chunk');
+const { asyncSequence } = require('./lib/asyncSequence/asyncSequence');
 
 module.exports = {
   call,
   attempt,
   chunk,
+  asyncSequence,
 };
